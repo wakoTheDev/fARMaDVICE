18266ee2daf7cfe11fe1cab39bb1040a0bd60061
wakoTheDev <162966971+wakoTheDev@users.noreply.github.com>
2024-07-15 13:19:30 +0000

    status component update

--- user-interface/src/status.js
+++ user-interface/src/status.js
@@ -11,59 +11,59 @@ const Status = () =>{
     
         <div style={{width:'65vw',padding:'1%',border:'1px solid #cccccc ',margin:'4px'}}>
         <section style={{width:'100%',display:'flex',padding:'5px',alignContent:'space between'}}>
-                <section style={{with:'30%',padding:'2px',fontSize:'0.8em',}}><p>Farm name:</p></section>
-                <section style={{with:'20%',padding:'2px',fontSize:'0.8em',marginLeft:'10px'}}><p>Duration:</p></section>
-                <section style={{with:'20%',padding:'2px',fontSize:'0.8em',marginLeft:'10px'}}><p>Saturation:</p></section>
-                <section style={{with:'10%',padding:'2px',fontSize:'0.8em',marginLeft:'10px'}}><p>Frequency:</p></section>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em',}}><p>Farm name:</p></section>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em'}}><p>Duration:</p></section>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em'}}><p>Saturation:</p></section>
+                <section style={{width:'15%',padding:'2px',fontSize:'0.8em'}}><p>Frequency:</p></section>
             </section>   
         <section style={{width:'100%',display:'flex',padding:'5px',margin:'4px'}}>
-            <section  style={{with:'40%',padding:'2px',fontWeight:'bold'}}>{createdSchedule.farmname[0]}</section>
-            <section style={{with:'30%',marginTop:'0',marginLeft:'15px',padding:'2px',fontWeight:'bold'}}>{createdSchedule.duration[0]}</section>
-            <section style={{with:'20%',marginTop:'0',marginLeft:'40px',padding:'2px',fontWeight:'bold'}}>{createdSchedule.saturation[0]}</section>
-            <section style={{with:'10%',marginTop:'0',marginLeft:'50px',padding:'2px',fontWeight:'bold'}}>{createdSchedule.frequency[0]}</section>
+            <section  style={{width:'25%',padding:'2px',fontWeight:'bold'}}>{createdSchedule.farmname[0]}</section>
+            <section style={{width:'25%',marginTop:'0',padding:'2px',fontWeight:'bold'}}>{createdSchedule.duration[0]}</section>
+            <section style={{width:'25%',marginTop:'0',padding:'2px',fontWeight:'bold'}}>{createdSchedule.saturation[0]}</section>
+            <section style={{width:'25%',marginTop:'0',padding:'2px',fontWeight:'bold'}}>{createdSchedule.frequency[0]}</section>
         </section>
-        <section style={{with:'100%',display:'flex'}}>
-        <button className='btn'>Activate</button>
-        <button className='btn'>Update</button>
-        <button className='btn'>Dectivate</button>
-        </section>
-        </div>
-        <div style={{width:'100%',padding:'1%',border:'1px solid #cccccc ',margin:'4px'}}>
-        <section style={{width:'100%',display:'flex',padding:'5px',alignContent:'space between'}}>
-                <section style={{with:'30%',padding:'2px',fontSize:'0.8em',}}><p>Farm name:</p></section>
-                <section style={{with:'20%',padding:'2px',fontSize:'0.8em',marginLeft:'10px'}}><p>Duration:</p></section>
-                <section style={{with:'20%',padding:'2px',fontSize:'0.8em',marginLeft:'10px'}}><p>Saturation:</p></section>
-                <section style={{with:'10%',padding:'2px',fontSize:'0.8em',marginLeft:'10px'}}><p>Frequency:</p></section>
-            </section>   
-        <section style={{width:'100%',display:'flex',padding:'5px'}}>
-            <section  style={{with:'40%',padding:'2px',fontWeight:'bold'}}>{createdSchedule.farmname[1]}</section>
-            <section style={{with:'30%',marginTop:'0',marginLeft:'15px',padding:'2px',fontWeight:'bold'}}>{createdSchedule.duration[1]}</section>
-            <section style={{with:'20%',marginTop:'0',marginLeft:'40px',padding:'2px',fontWeight:'bold'}}>{createdSchedule.saturation[1]}</section>
-            <section style={{with:'10%',marginTop:'0',marginLeft:'50px',padding:'2px',fontWeight:'bold'}}>{createdSchedule.frequency[1]}</section>
-        </section>
-        <section style={{with:'100%',display:'flex'}}>
-        <button className='btn'>Activate</button>
-        <button className='btn'>Update</button>
-        <button className='btn'>Dectivate</button>
+        <section style={{width:'100%',display:'flex'}}>
+        <button className='btn' style={{height:"30px",borderRadius:"4px"}}>Activate</button>
+        <button className='btn' style={{height:"30px",borderRadius:"4px"}}>Update</button>
+        <button className='btn' style={{height:"30px",borderRadius:"4px"}}>Dectivate</button>
         </section>
         </div>
-        <div style={{width:'100%',padding:'1%',border:'1px solid #cccccc ',margin:'4px'}}>
+        <div style={{width:'65vw',padding:'1%',border:'1px solid #cccccc ',margin:'4px'}}>
         <section style={{width:'100%',display:'flex',padding:'5px',alignContent:'space between'}}>
-                <section style={{with:'30%',padding:'2px',fontSize:'0.8em',}}><p>Farm name:</p></section>
-                <section style={{with:'20%',padding:'2px',fontSize:'0.8em',marginLeft:'10px'}}><p>Duration:</p></section>
-                <section style={{with:'20%',padding:'2px',fontSize:'0.8em',marginLeft:'10px'}}><p>Saturation:</p></section>
-                <section style={{with:'10%',padding:'2px',fontSize:'0.8em',marginLeft:'10px'}}><p>Frequency:</p></section>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em',}}><p>Farm name:</p></section>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em' }}><p>Duration:</p></section>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em' }}><p>Saturation:</p></section>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em' }}><p>Frequency:</p></section>
             </section>   
         <section style={{width:'100%',display:'flex',padding:'5px'}}>
-            <section  style={{with:'40%',padding:'2px',fontWeight:'bold'}}>{createdSchedule.farmname[2]}</section>
-            <section style={{with:'30%',marginTop:'0',marginLeft:'15px',padding:'2px',fontWeight:'bold'}}>{createdSchedule.duration[2]}</section>
-            <section style={{with:'20%',marginTop:'0',marginLeft:'40px',padding:'2px',fontWeight:'bold'}}>{createdSchedule.saturation[2]}</section>
-            <section style={{with:'10%',marginTop:'0',marginLeft:'50px',padding:'2px',fontWeight:'bold'}}>{createdSchedule.frequency[2]}</section>
+            <section  style={{width:'25%',padding:'2px',fontWeight:'bold'}}>{createdSchedule.farmname[1]}</section>
+            <section style={{width:'25%',marginTop:'0' ,padding:'2px',fontWeight:'bold'}}>{createdSchedule.duration[1]}</section>
+            <section style={{width:'25%',marginTop:'0' ,padding:'2px',fontWeight:'bold'}}>{createdSchedule.saturation[1]}</section>
+            <section style={{width:'25%',marginTop:'0' ,padding:'2px',fontWeight:'bold'}}>{createdSchedule.frequency[1]}</section>
         </section>
         <section style={{with:'100%',display:'flex'}}>
-        <button className='btn'>Activate</button>
-        <button className='btn'>Update</button>
-        <button className='btn'>Dectivate</button>
+        <button className='btn' style={{height:"30px",borderRadius:"4px"}}>Activate</button>
+        <button className='btn' style={{height:"30px",borderRadius:"4px"}}>Update</button>
+        <button className='btn' style={{height:"30px",borderRadius:"4px"}}>Dectivate</button>
+        </section>
+        </div>
+        <div style={{width:'65vw',padding:'1%',border:'1px solid #cccccc ',margin:'4px'}}>
+        <section style={{width:'100%',display:'flex',padding:'5px',alignContent:'space between'}}>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em',}}><p>Farm name:</p></section>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em' }}><p>Duration:</p></section>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em '}}><p>Saturation:</p></section>
+                <section style={{width:'25%',padding:'2px',fontSize:'0.8em' }}><p>Frequency:</p></section>
+            </section>   
+        <section style={{width:'100%',display:'flex',padding:'5px'}}>
+            <section  style={{width:'25%',padding:'2px',fontWeight:'bold'}}>{createdSchedule.farmname[2]}</section>
+            <section style={{width:'25%',marginTop:'0' ,padding:'2px',fontWeight:'bold'}}>{createdSchedule.duration[2]}</section>
+            <section style={{width:'25%',marginTop:'0' ,padding:'2px',fontWeight:'bold'}}>{createdSchedule.saturation[2]}</section>
+            <section style={{width:'25%',marginTop:'0' ,padding:'2px',fontWeight:'bold'}}>{createdSchedule.frequency[2]}</section>
+        </section>
+        <section style={{width:'100%',display:'flex'}}>
+        <button className='btn' style={{height:"30px",borderRadius:"4px"}}>Activate</button>
+        <button className='btn' style={{height:"30px",borderRadius:"4px"}}>Update</button>
+        <button className='btn' style={{height:"30px",borderRadius:"4px"}}>Dectivate</button>
         </section>
         </div>
     </>
